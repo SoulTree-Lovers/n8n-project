d5b5b3860b0bc40c47487984179eabe003d8f0ad
강승민 <kangseungmin@gangseungmin-ui-Macmini.local>
2025-09-13 23:54:17 +0900

    [feat] health check controller

---
+++ src/main/java/com/example/n8n_cicd/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.n8n_cicd;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class HealthCheckController {
+
+    @RequestMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
